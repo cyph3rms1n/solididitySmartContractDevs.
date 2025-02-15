22f178b50a81eb9bce98d3acdcc2af3b1b15df51
Jyotish kumar yadav <22bcs12392@cuchd.in>
2025-02-16 01:15:38 +0530

    Update SimpleStorage.sol

--- First/SimpleStorage.sol
+++ First/SimpleStorage.sol
@@ -4,25 +4,37 @@ pragma solidity ^0.8.18;
 contract SimpleStorage{
 
 
-    uint256 public favoriteNumber; // uint256 variable type
+    uint256  myFavoriteNumber; // uint256 variable type
 
     // creating a fuction which take a parameter uint256 and set the value of the variable "favoriteNumber"
-
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
+        myFavoriteNumber = _favoriteNumber;
     }
-
-
-    /*
-        Pure function declares that no state variable will be changed or read.
-    */
-    
-    // Creating a getter function which returns a value of uint256.
-    /*
-        View function declares that no state will be changed and only can view the variable
-    */
+    // View and Pure
     function retrieve() public  view  returns(uint256){
-        return favoriteNumber;
+        return myFavoriteNumber;
     } 
 
+
+    // Creatign a user-defined variable : Struct
+    // same as : uint256[] listOfFavouriteNumbers;
+    /*
+        * First: we created a variable name "Person"
+        * Second: we created an array of the variable "person" and name the variable as "listOfPerson"
+        * Third: we created a function called "addPerson" to add the value to the array "listOfPerson"
+    */
+    struct Person{
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeoples; // holds null as a default value and also this array is a dynamic array
+
+    function addPerson(uint256 _favouriteNumber,string memory _name) public {
+        listOfPeoples.push( Person(_favouriteNumber,_name));
+    }
+    // Person public sam = Person(99,"sam");
+    // Person public noah = Person(65, "noah");
+    // Person public ryuk = Person({favouriteNumber: 32, name : "ryuk"});
+    // Person public mykex = Person({favouriteNumber: 7, name : "mykex"});
 }
