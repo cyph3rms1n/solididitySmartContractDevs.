46b0adf0fa4c90173aaf40ad87793301e7ad3dc5
Jyotish kumar yadav <22bcs12392@cuchd.in>
2025-02-21 00:04:48 +0530

    Update FundMe.sol

--- Remix-Fund-me/FundMe.sol
+++ Remix-Fund-me/FundMe.sol
@@ -29,7 +29,16 @@ contract fundMe {
         // revert: undo the prior operation if the transection fails (condition didn't meet) and returns the remaining gas fees.
         funders.push(msg.sender);
         // Keeps the track of funders who funds this contract.
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        addressToAmountFunded[msg.sender] +=  msg.value;
+    }
+
+    function withdraw() public {
+        // Using for loop to reset the funded amount after the withdraw function executes
+
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex ++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
     }
 
    
